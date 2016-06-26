4ed8abcad95e8b4567ce99020d1dd38f4f9e1c8c
igorwanbarros <igor.wanderley@hotmail.com>
2016-06-26 15:07:48 -0300

    adicionando classe TagForm

---
+++ app/HtmlViews/Forms/TagForm.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\HtmlViews\Forms;
+
+
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Hidden;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Html;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Text;
+use Igorwanbarros\ViewDevelopPhp\Form\Form;
+
+class TagForm extends Form
+{
+    public function toStart()
+    {
+        $this->setMethod('POST')
+            ->setAction(url('tags/salvar'))
+            ->addAttributes('class', 'ui form')
+            ->setFront('semantic')
+            ->addField(Hidden::source('id'))
+            ->addField(Text::source('nome', 'Tag')->setSize(9))
+            ->addField(Text::source('tipo', 'Tipo')->setSize(9))
+            ->addField(Html::source('btn_salvar', 'button', '<div class="hidden content">Salvar</div><div class="visible content"><i class="save icon"></i></div>')
+                ->addAttributes('class', 'ui vertical black animated button')
+                ->addAttributes('type', 'submit')
+                ->setSize(18)
+            );
+
+        return $this;
+    }
+}
