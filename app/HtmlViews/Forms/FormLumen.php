f51373fd228eb411fd38b5d56b4a5b769867e0e7
igorwanbarros <igorwanbarros@gmail.com>
2016-08-05 20:47:57 -0300

    adicionando formulario de consulta no FormLaravel

--- app/HtmlViews/Forms/FormLumen.php
+++ app/HtmlViews/Forms/FormLumen.php
@@ -1,12 +1,14 @@
 <?php
 
-
 namespace App\HtmlViews\Forms;
 
-
-use Igorwanbarros\ViewDevelopPhp\Form\Field;
-use Igorwanbarros\ViewDevelopPhp\Form\Fields\Hidden;
+use Igorwanbarros\ViewDevelopPhp\TagHtml;
 use Igorwanbarros\ViewDevelopPhp\Form\Form;
+use Igorwanbarros\ViewDevelopPhp\Form\Field;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Html;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Text;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Select;
+use Igorwanbarros\ViewDevelopPhp\Form\Fields\Hidden;
 
 class FormLumen extends Form
 {
@@ -28,6 +30,35 @@ class FormLumen extends Form
         $this->addAttribute('class', 'ui form')
             ->setFront('semantic')
             ->setBasePath(base_path('app/HtmlViews/Forms/'));
+    }
+
+
+    public function search(array $campos = [])
+    {
+        $this->fields = [];
+        $this->action = str_replace('/salvar', '', $this->action);
+        $this->setMethod('GET');
+
+        $input      = Text::source('search', false)->addAttributes('placeholder', 'Pesquisar...');
+        $button     = TagHtml::source('button', '<i class="search icon"></i>', ['class' => 'ui icon button black']);
+        $divSearch  = Html::source('search_div', 'div', $input . $button)
+            ->addAttributes('class', 'ui right action input')
+            ->setSize(8);
+
+        $this->addField(
+            Select::source('filters', false)
+                ->addAttributes('placeholder', 'Selecione Coluna')
+                ->addAttributes('class', 'search fluid')
+                ->addAttributes('multiple', 'multiple')
+                ->addAttributes('name', 'filters[]')
+                ->addAttributes('id', 'filters')
+                ->addOptions($campos)
+                ->setDefaultOption('Todos')
+                ->setSize(8)
+        );
+        $this->addField($divSearch);
+
+        return $this;
     }
 
 
